d728499666a85257e5cf693c6c2b3500e01d42a5
Sumit-241 <skicholiya2002@gmail.com>
2022-08-20 21:53:45 +0530

    second change

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://glimpses7.herokuapp.com/posts' })
+const API = axios.create({ baseURL: 'https://glimpses7.herokuapp.com' })
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
